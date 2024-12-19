f5707601b50e81ee7dcfb0404b1036382f39e6f8
ramya72002 <nsriramya7@gmail.com>
2024-12-19 10:59:28 +0530

    master list

---
+++ src/app/(MainBody)/users copy/mpes/page.tsx
@@ -0,0 +1,8 @@
+import UserCardsContainer from "@/Components/Application/Users/UserCards/UserCards";
+import PersonalTab from "@/Components/Application/MasterList/TabComponent/PersonalTab/PersonalTab";
+
+const UserCards = () => {
+  return <PersonalTab/>
+};
+
+export default UserCards;
