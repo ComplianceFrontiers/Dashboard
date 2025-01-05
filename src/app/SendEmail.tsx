72c46d7a0533e94d658a766d4a4fce16c62960af
ramya72002 <nsriramya7@gmail.com>
2025-01-05 17:46:34 +0530

    done

--- src/app/SendEmail.tsx
+++ src/app/SendEmail.tsx
@@ -28,207 +28,115 @@ const SendEmail = ({ selectedEmails, onClose }: { selectedEmails: string[]; onCl
   const [apiLink, setApiLink] = useState('');
 
   const emailBody = `
-<div style="font-family: Arial, sans-serif; color: #333; padding: 20px; text-align: center; display: block;">
-    <div style="text-align: center;">
-      <img src="https://res.cloudinary.com/dtgje24ez/image/upload/v1736074851/pkqpnwtoric5y02q5tsj.jpg" alt="Chess Champs Banner" style="max-width: 100%; height: auto;">
-    </div>
-    <h1 style="color: #000; text-align: left; font-size: 14px; font-weight: bold; margin-bottom: 20px;margin-top: 20px;">
-      Now enrolling for the 2025 Winter/Spring Session of Chess Champs, beginning January 21, 2025, and concluding with a showcase on Sunday, April 27, 2025, at 3 PM.
-    </h1>
-    <p style="font-size: 14px; line-height: 1.6; margin-top: 20px; margin-bottom: 10px; text-align: left; max-width: 800px;   margin-right: auto;">
-      This program, run by our talented JCoaches, is designed to inspire dancers of all levels. Whether you're just starting out or looking to build upon your existing skills, our classes offer something for everyone.
-    </p>
-
-    <!-- Programs -->
-    <h2 style="text-align: left; color: #f53db8; margin-bottom: 10px;">Programs:</h2>
-   <ul style="text-align: left;   font-size: 14px; line-height: 1.6; list-style-type: disc;">
-  <li><strong>Creative Movement</strong> (Ages 4 - 6)</li>
-  <li><strong>Ballet 1</strong> (Ages 7 - 9)</li>
-  <li><strong>Ballet 2</strong> (Ages 10 - 12)</li>
-  <li><strong>Jazz</strong> (Ages 7 - 12)</li>
-  <li><strong>Adult Beginner Ballet</strong> (Ages 18+)</li>
-  <li><strong>Adult Intermediate Ballet</strong> (Ages 18+)</li>
-</ul>
-
-    <p style="font-size: 14px; line-height: 1.6; margin-top: 20px; margin-bottom: 10px; text-align: left; max-width: 800px;  margin-right: auto;">
-   Check out our schedule & class descriptions below. Join us and discover the joy of dance in a supportive and creative environment. We can’t wait to see you there!
-    </p>
-
-    <h2 style="text-align: left; color: #f53db8; margin-bottom: 10px;">Cost:</h2>
-    <ul style="text-align: left;list-style-type: disc; padding-left: 50px; font-size: 14px; line-height: 1.6;">
-      <li>Once weekly programs: $150/Member, $200/Non-Member</li>
-      <li>Twice weekly programs: $250/Member, $300/Non-Member</li>
-    </ul>
-
-    <div style="text-align: center; margin-top: 30px;">
-      <a href="https://chesschampsus.vercel.app/unsubscribe" style="display: inline-block; padding: 12px 25px; background-color: #f53db8; color: white; text-align: center; border-radius: 5px; text-decoration: none; font-size: 16px;">
-        REGISTER HERE
-      </a>
-    </div>
-
-    <h2 style="text-align: left; color: #f53db8; margin-top: 20px; margin-bottom: 10px;">SHOWCASE:</h2>
-    <p style="font-size: 14px; line-height: 1.6; margin-top: 20px; margin-bottom: 10px; text-align: left; max-width: 800px;  margin-right: auto;">
-Interested in getting a sneak peek at our program? Come check out the Showcase for the current session! It will be held on <strong>Sunday, December 22 at 3 PM</strong> in the <strong>Auditorium</strong>.
-</p>
-
-
-    <!-- Schedule -->
-    <h2 style="text-align: left; color: #ffffff; padding:10px; margin-top: 20px; margin-bottom: 10px; background-color: #000000;">Schedule:</h2>
-   <ul style="text-align: left; padding-left: 0px; font-size: 14px; line-height: 1.6; list-style: disc;">
- <b style="color: #f53db8; font-size: 16px;">Creative Movement (Ages 4 - 6)</b>
-<ul style="padding-left: 30px; list-style-type: disc;">
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">K/1st Graders on Tuesdays</a></strong>, 
-    Jan 21-Apr 22, 4:45-5:30 PM with Ms. Brenna in Studio 2
-  </li>
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Pre-K on Thursdays</a></strong>: 
-    Jan 23-Apr 24, 4:45-5:30 PM with Ms. Sam in Studio 2
-  </li>
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Combined Ages 4-6 on Saturdays</a></strong>: 
-    Jan 25-Apr 26, 9:15-10 AM with Ms. Sarah in the Mercaz
-  </li>
-</ul>
-
-<b style="color: #f53db8; font-size: 16px;">Ballet 1 (Ages 7 - 9)</b>
-<ul style="padding-left: 30px; list-style-type: disc;">
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Tuesdays</a></strong>, 
-    Jan 21-Apr 22, 5:45-6:45 PM with Ms. Brenna in the Mercaz
-  </li>
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Saturdays</a></strong>, 
-    Jan 25-Apr 26: 11:15 AM-12:15 PM with Ms. Sarah in the Mercaz
-  </li>
-</ul>
-
-<b style="color: #f53db8; font-size: 16px;">Ballet 2 (Ages 10 - 12)</b>
-<ul style="padding-left: 30px; list-style-type: disc;">
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Thursdays & Sundays</a></strong>, 
-    Jan 23-Apr 20: 6:45-8 PM Thursdays & 11 AM-12:15 PM Sundays with Mr. Leo in the Mercaz
-  </li>
-</ul>
-
-<b style="color: #f53db8; font-size: 16px;">Jazz (Ages 7 - 12)</b>
-<ul style="padding-left: 30px; list-style-type: disc;">
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Tuesdays</a></strong>, 
-    Jan 21-Apr 22: 7-8 PM with Ms. Brenna in the Mercaz
-  </li>
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Saturdays</a></strong>, 
-    Jan 25-Apr 26: 10-11 AM with Ms. Sarah in the Mercaz
-  </li>
-</ul>
-
-<b style="color: #f53db8; font-size: 16px;">Adult Ballet (Ages 18+)</b>
-<ul style="padding-left: 30px; list-style-type: disc;">
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Beginner on Wednesdays</a></strong>, 
-    Jan 22-Apr 23, 7-8 PM with Mr. Leo in Studio 2
-  </li>
-  <li style="padding-left: 10px;">
-    <strong><a href="#" style="color: blue; text-decoration: underline;">Intermediate on Sundays</a></strong>, 
-    Jan 26-Apr 20, 8:45-9:45 AM with Mr. Leo in the Mercaz
-  </li>
-</ul>
-<div style="text-align: center; margin-top: 30px;">
-      <a href="https://chesschampsus.vercel.app/unsubscribe" style="display: inline-block; padding: 12px 25px; background-color: #f53db8; color: white; text-align: center; border-radius: 5px; text-decoration: none; font-size: 16px;">
-        REGISTER HERE
-      </a>
-    </div>
-
-
-    <!-- Descriptions -->
-<h2 style="text-align: left; color: #ffffff; padding:10px; margin-top: 20px; margin-bottom: 10px; background-color: #000000;">Class Descriptions:</h2>    <ul style="text-align: left; padding-left: 20px; font-size: 14px; line-height: 1.6;">
-   <ul style="text-align: left; font-size: 14px; line-height: 1.6; list-style-type: disc; padding-left: 0px;">
-  <li>
-    <strong>Creative Movement (Ages 4 - 6):</strong> A prerequisite dance class. Students learn basics of stretching, foundational dance steps, and musical phrasing through a series of guided "imagination games." A great way to learn the foundations of dance that apply for any future dance classes.
-  </li>
-  <li>
-    <strong>Ballet 1 (Ages 7 - 9):</strong> Students learn the foundational steps of ballet. Classes include stretching and conditioning, barre and center exercises, and basics of ballet jumps and turns.
-  </li>
-  <li>
-    <strong>Ballet 2 (Ages 10 - 12):</strong> Students begin learning intermediate level barre, center, and across the floor combinations emphasizing musical phrasing, correct placement, and coordination. Students also begin learning the basics of pointe technique (in flat shoes until they are ready for pointe shoes).
-  </li>
-  <li>
-    <strong>Jazz (Ages 7 - 12):</strong> A combination of ballet and modern dance technique blended with current popular dance steps, Broadway-style dance, and classic American dance forms and music.
-  </li>
-  <li>
-    <strong>Adult Beginner Ballet (Ages 18+):</strong> A ballet basics class intended for adults with little or no experience, ages 18 and up.
-  </li>
-  <li>
-    <strong>Adult Intermediate Ballet (Ages 18+):</strong> A ballet class intended for adults with some ballet experience, ages 18 and up.
-  </li>
-</ul>
-
-
-<p style="text-align:center; margin-top:10px;">
-  For more info, please contact 
-  <a href="#" style="text-decoration: underline; color: blue;">
-    Steph Kegelman
-  </a>.
-</p>
-
-
-
-    <!-- Footer -->
-    <div style="color: white; padding: 0px; margin-top: 30px;">   
+  <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; text-align: center; display: block;">
+      <div style="text-align: center;">
+        <img src="https://res.cloudinary.com/dtgje24ez/image/upload/v1736074851/pkqpnwtoric5y02q5tsj.jpg" alt="Chess Champs Banner" style="max-width: 100%; height: auto;">
+      </div>
+      <h1 style="color: #000; text-align: left; font-size: 14px; font-weight: bold; margin-bottom: 20px; margin-top: 20px;">
+        Now Enrolling for the 2025 Delaware Chess Club Championship!
+      </h1>
+      <p style="font-size: 14px; line-height: 1.6; margin-top: 20px; margin-bottom: 10px; text-align: left; max-width: 800px; margin-right: auto;">
+        <strong> Tournament Date: </strong> Saturday, January 11, 2025, from 1:30 PM to 4:30 PM.<br>
+        <strong> Location: </strong> Kathleen H. Wilbur Elementary School, 4050 Wrangle Hill Rd, Bear, DE 19701.
+      </p>
+      <p style="font-size: 14px; line-height: 1.6; margin-top: 10px; margin-bottom: 20px; text-align: left; max-width: 800px; margin-right: auto;">
+        This exciting chess event offers opportunities for both competitive and casual players, ensuring a fun and enriching experience for all!
+      </p>
+        <h2 style="text-align: left; font-size: 16px; color: #f53db8; margin-bottom: 10px; margin-left: 20px;"><strong>PLAY CATEGORIES:</strong></h2>
  
-        <div style="background-color: #343a40; color: #fff; padding: 20px; border-radius: 5px 5px 0 0;">
-            <h1 style="margin-bottom: 5px; font-size: 14px">About the Chess Champs</h1>
-            <p style="margin-bottom: 0px; font-size:12px">The Siegel JCC is a non-profit Jewish community center where people of all faiths and backgrounds come together to learn, share, play, and celebrate. Our 32-acre campus houses an Early Childhood Center, Youth & Teen Programs, and Summer Camp, as well as a state-of-the-art Fitness Center, Indoor & Outdoor Pools, Sports, and Community Events.</p>
-        </div>
-    <div >
-         <div style="background-color: #007BFF; padding: 20px; border-radius: 5px; text-align: center;">
-    <p style="margin: 0;">SIEGEL JCC</p>
-  
-               <p style="margin: 0;font-size:10px">
-  <a  href="https://www.google.com/maps?q=101+Garden+of+Eden+Road,+Wilmington,+DE+19803" target="_blank" style="text-decoration: underline;font-size:12px; color:#ffff00;">101 Garden of Eden Road</a> | 
-  <a href="tel:+13024785660" style="text-decoration: underline;font-size:12px; color:#ffff00;">Wilmington, DE 19803</a> | 
-  <a href="tel:+13024785660">302-478-5660</a>
-</p>
+<ul style="font-size: 14px; line-height: 1.6; text-align: left; max-width: 800px; margin-left: 20px; margin-right: auto; list-style-type: disc; padding-left: 20px;">
+  <li style="margin-bottom: 10px;">
+    <strong>Rated Section:</strong>
+    <ul style="list-style-type: disc; margin-left: 20px;">
+      <li><strong>Open (Rated Only):</strong> Compete for the <em>Delaware Chess Club Champion</em> title.</li>
+      <li><strong>K-12 Championship (Rated):</strong> Compete for the <em>Delaware Jr. Club Champion</em> title.</li>
+    </ul>
+  </li>
+  <li>
+    <strong>Casual Section:</strong>
+    <ul style="list-style-type: disc; margin-left: 20px;">
+      <li><strong>K-5 Championship (Casual Only):</strong> Compete for the <em>Scholastic Jr. Club Champion</em> title.</li>
+      <li><strong>K-12 Championship (Casual):</strong> Compete for the <em>Scholastic Club Champion</em> title.</li>
+    </ul>
+  </li>
+</ul>
+ <div style="text-align: center; margin-top: 30px;">
+        <a href="https://chesschampsus.vercel.app/register" style="display: inline-block; padding: 12px 25px; background-color: #f53db8; color: white; text-align: center; border-radius: 5px; text-decoration: none; font-size: 16px;">
+          REGISTER HERE
+        </a>
+      </div>
+<strong>
+      <h3 style="color: #000; text-align: left; font-size: 14px; margin-top: 20px; margin-bottom: 20px;"><strong>TOURNAMENT FORMAT:</strong></h3>
 
+     <ul style="font-size: 14px; line-height: 1.6; text-align: left; max-width: 800px; margin-left: 20px;">
+  <li>4 Rounds | G/25 Time Control (Game in 25 minutes).</li>
+  <li>
+    Awards:
+    <ul style="margin-left: 20px;">
+      <li>🏆 Winner: Trophy</li>
+      <li>🥈 2nd Place: Medal</li>
+      <li>🥉 3rd Place: Medal</li>
+    </ul>
+  </li>
+  <li>Registration Fee: $25 (Online registration only; no onsite registration available).</li>
+</ul>
 
-            </div>
-
-            <div>
-                <p style="font-size: 11px;  ">We like to connect in ways that work for you: <a href="#" style="color: #fff; text-decoration: none;">Update Profile</a></p>
-            </div>
-        </div>
-    </div>
-</div>
-<p style="font-size: 14px; color: #666; text-align: center; margin-top: 0px;">
-    You are receiving this email because you signed up to receive updates and communications from 
-    <a href="https://chesschamps.us" style="color: #f53db8; text-decoration: none;">Chess Champs</a>. 
-    If you wish to stop receiving these emails, you can 
-    <a href="https://chesschampsus.vercel.app/unsubscribe" style="color: #f53db8; text-decoration: none;">unsubscribe here</a>.
-</p>
-
+      </strong>
+      <h3 style="color: #000; text-align: left; font-size: 14px; margin-top: 20px; margin-bottom: 20px;"><strong>TROPHY TITLES:</strong></h3>
+      <ul style="font-size: 14px; line-height: 1.6; text-align: left; max-width: 800px; margin-left: 20px; margin-right: auto; list-style-type: disc; padding-left: 20px;">
+  <li style="margin-bottom: 10px;">
+    <strong>Rated Section:</strong>
+    <ul style="list-style-type: disc; margin-left: 20px;">
+      <li><strong>Delaware Chess Club Champion:</strong> (Open). </li>
+      <li><strong>Delaware Jr. Club Champion:</strong> (K-12). </li>
+    </ul>
+  </li>
+  <li>
+    <strong>Casual Section:</strong>
+   <ul style="list-style-type: disc; margin-left: 20px;">
+      <li><strong>Scholastic Club Champion:</strong> (k-12). </li>
+      <li><strong>Scholastic Jr. Club Champion:</strong> (K-5). </li>
+    </ul>
+  </li>
+</ul>
+ <div style="text-align: center; margin-top: 30px;">
+        <a href="https://chesschampsus.vercel.app/register" style="display: inline-block; padding: 12px 25px; background-color: #f53db8; color: white; text-align: center; border-radius: 5px; text-decoration: none; font-size: 16px;">
+          REGISTER NOW
+        </a>
+      </div>
+            <p style="font-size: 14px; color: #666; text-align: center; margin-top: 5px;">Spots are limited, so secure yours today and join us for an unforgettable chess experience!</p>
+<p><strong>For more information,</strong> contact [connect@chesschamps.us].</p>
+      
+      <div style="color: white; padding: 0px; margin-top: 30px;">   
+          <div style="background-color: #343a40; color: #fff; padding: 20px; border-radius: 5px 5px 0 0;">
+              <h1 style="margin-bottom: 5px;color:white; font-size: 14px;">About the Chess Champs</h1>
+              <p style="margin-bottom: 0px; font-size:12px;">Chess Champs is dedicated to nurturing young minds through the timeless game of chess. We provide engaging programs, tournaments, and training sessions for players of all ages and skill levels. Our mission is to inspire critical thinking, strategic planning, and a love for learning in a supportive and fun environment.</p>
+              <p style="margin-bottom: 0px; font-size:12px;">From beginner-friendly lessons to competitive tournaments, Chess Champs offers something for everyone, helping players build confidence, sharpen problem-solving skills, and foster a lifelong passion for chess.</p>
+          </div>
+          <div>
+              <div style="background-color: #007BFF; padding: 20px; border-radius: 5px; text-align: center;">
+                  <p style="margin: 0;">CHESS CHAMPS</p>
+                  <p style="margin: 0; font-size: 10px;">
+                      <a href="https://www.google.com/maps?q=510+Duncan+Rd,+Wilmington,+DE+19809" target="_blank" style="text-decoration: underline; font-size: 12px; color: #ffff00;">510 Duncan Rd</a> | 
+                      <a href="tel:+13022764141" style="text-decoration: underline; font-size: 12px; color: #ffff00;">Wilmington, DE 19809</a> | 
+                      <a href="tel:+13022764141" style="text-decoration: underline;font-size: 12px; color: #ffff00;">302-276-4141</a>
+                  </p>
+              </div>
+              <div>
+                  <p style="font-size: 11px;">We like to connect in ways that work for you: <a href="#" style="color: #fff; text-decoration: none;">Update Profile</a></p>
+              </div>
+          </div>
+      </div>
+      <p style="font-size: 14px; color: #666; text-align: center; margin-top: 0px;">
+          You are receiving this email because you signed up to receive updates and communications from 
+          <a href="https://chesschamps.us" style="color: #f53db8; text-decoration: none;">Chess Champs</a>. 
+          If you wish to stop receiving these emails, you can 
+          <a href="https://chesschampsus.vercel.app/unsubscribe" style="color: #f53db8; text-decoration: none;">unsubscribe here</a>.
+      </p>
   </div>
-`;
+  `;
 
-// <p style="display: flex; justify-content: center; align-items: center; gap: 10px; margin: 0;">
-// <a href="https://www.facebook.com" target="_blank">
-//     <img src="https://res.cloudinary.com/dtgje24ez/image/upload/v1734435408/radiw6odgmhlnh82bxcz.png" alt="Facebook" style="width: 20px; height: 20px;" />
-// </a>
-// <a href="https://www.youtube.com" target="_blank">
-//     <img src="https://res.cloudinary.com/dtgje24ez/image/upload/v1734435408/weqk53gu7f0km5fq1mo2.png" alt="YouTube" style="width: 20px; height: 20px;" />
-// </a>
-// <a href="https://www.instagram.com" target="_blank">
-//     <img src="https://res.cloudinary.com/dtgje24ez/image/upload/v1734435408/mssyh3o9t8pigtiumszb.png" alt="Instagram" style="width: 20px; height: 20px;" />
-// </a>
-// <a href="https://www.chesschamps.us/" target="_blank">
-//     <img src="https://res.cloudinary.com/dtgje24ez/image/upload/v1734435408/gsdc2rxtvnjw7nqs4m4r.png" alt="Chess Champs" style="width: 20px; height: 20px;" />
-// </a>
-// <a href="#" target="_blank">
-//     <img src="https://res.cloudinary.com/dtgje24ez/image/upload/v1734435408/qnazc8aquouxkwensstj.png" alt="LinkedIn" style="width: 20px; height: 20px;" />
-// </a>
-// </p>
-
-
+  
 
 
 const [emails, setEmails] = useState<string[]>(selectedEmails);
