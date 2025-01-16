0c8925a7400062738b4e86687309b53f4e0d93b2
TanujKumar41 <tanujkumar191001@gmail.com>
2025-01-16 12:59:01 +0530

    changes

---
+++ src/app/(MainBody)/masterlist/basicsofchess/page.tsx
@@ -0,0 +1,7 @@
+import UserCardsContainer from "@/Components/Application/Users/UserCards/UserCards";
+import PersonalTab from "@/Components/Application/MasterList/TabComponent/PersonalTab/PersonalTab9";
+const UserCards = () => {
+  return <PersonalTab/>
+};
+
+export default UserCards;
