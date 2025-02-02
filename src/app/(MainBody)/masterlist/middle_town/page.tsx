bb7f3be2767d0e44a6ee7c4a8ddce7f334ba6c74
ramya72002 <nsriramya7@gmail.com>
2025-02-02 10:07:07 +0530

    Readme file updated

--- src/app/(MainBody)/masterlist/middle_town/page.tsx
+++
@@ -1,7 +0,0 @@
-import UserCardsContainer from "@/Components/Application/Users/UserCards/UserCards";
-import PersonalTab10 from "@/Components/Application/MasterList/TabComponent/PersonalTab/PersonalTab10";
-const UserCards = () => {
-  return <PersonalTab10/>
-};
-
-export default UserCards;
